03b470e52054b550b90379e5f8377537a3e90c16
TTrld <thomas.terlinden@ensea.fr>
2024-12-03 10:28:44 +0100

    q2.a

--- tp1project/main.c
+++ tp1project/main.c
@@ -2,22 +2,45 @@
 #include "string.h"
 #include <stdio.h>
 #include <sys/types.h>
-//#include <sys/wait.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #define BUFSIZE 128
 
+int fd;
+char buf[BUFSIZE];
+ssize_t ret;
+
+char Shellstart[BUFSIZE] = "$ ./enseash\n";
+char Outputestline1[BUFSIZE] = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
+char Endshell[BUFSIZE] = "enseash %\n";
+char Command[BUFSIZE];
+
+
 int main(int argc, char* argv[]) {
-
-    int fd;
-    char buf[BUFSIZE];
-    ssize_t ret;
-
-    char shellstart[BUFSIZE] = "$ ./enseash\n";
-    char Outputestline1[BUFSIZE] = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
-    char endshell[BUFSIZE] = "enseash %\n";
-
-    write(STDOUT_FILENO, shellstart, strlen(shellstart));
+	write(STDOUT_FILENO, Shellstart, strlen(Shellstart));
     write(STDOUT_FILENO, Outputestline1, strlen(Outputestline1));
-    write(STDOUT_FILENO, endshell, strlen(endshell));
+    
+    while(1){
+		ret = read(STDIN_FILENO,Command,BUFSIZE);
+		if (ret < 0){
+			perror("command nulle");
+			exit(EXIT_FAILURE);
+		}
+		
+		if (ret > 0 && Command[ret - 1] == '\n') {
+			Command[ret - 1] = '\0';
+        }
+        
+        Command[ret]='\0';
+        
+		if (strcmp(Command ,"exit")==0){
+			write(STDOUT_FILENO,Endshell,strlen(Endshell));
+			break;
+		}else {
+			write(STDOUT_FILENO,"Commande inconnue \n",BUFSIZE);
+		}
+	}
+	return 0;
 }
