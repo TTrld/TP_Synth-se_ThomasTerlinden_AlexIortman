d4fa09b94f484f16385aa55835eef7596c790f11
A1eX120 <alex.iortoman@ensea.fr>
2024-12-03 11:39:29 +0100

    Update TP1 q2.c
    Adding comments

--- TP1 q2.c
+++ TP1 q2.c
@@ -15,46 +15,47 @@ ssize_t ret;
 char Shellstart[BUFSIZE] = "$ ./enseash\n";
 char Outputestline1[BUFSIZE] = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
 char Endshell[BUFSIZE] = "enseash %\n";
-char Command[BUFSIZE];
+char command[BUFSIZE];
 
 
 int main(int argc, char* argv[]) {
-	write(STDOUT_FILENO, Shellstart, strlen(Shellstart));
+	
+    write(STDOUT_FILENO, Shellstart, strlen(Shellstart));
     write(STDOUT_FILENO, Outputestline1, strlen(Outputestline1));
     
     while(1){
-		ret = read(STDIN_FILENO,Command,BUFSIZE);
-		if (ret < 0){
-			perror("unknown command");
-			exit(EXIT_FAILURE);
-		}
+	ret = read(STDIN_FILENO,command,BUFSIZE);
+	if (ret < 0){
+		perror("unknown command");
+		exit(EXIT_FAILURE);
+	}
 		
-		if (ret > 0 && Command[ret - 1] == '\n') {
-			Command[ret - 1] = '\0';
+	if (ret > 0 && command[ret - 1] == '\n') {
+		command[ret - 1] = '\0';
         }
         
-        Command[ret]='\0';
+        command[ret]='\0'; //To avoid segmentation fault
         
-		if (strcmp(Command ,"exit")==0){
-			break;
-		}
+	if (strcmp(command ,"exit")==0){
+		break;
+	}
 		
-		pid_t pid = fork();
+	pid_t pid = fork();
         if (pid < 0) {
-            perror("Erreur fork");
+            perror("Error : fork");
             exit(EXIT_FAILURE);
-        }
-		
-
+	}
+	// child program to execute a shell command (transfer of execution) while the father runs. 
         if (pid == 0) {
-            execlp(Command, Command, (char *)NULL);
-            perror("Erreur d'exécution de la commande");
+            execlp(Command, Command, (char *)NULL); // execution of Command
+	    perror("Command execution error");
             exit(EXIT_FAILURE);
-        } else {
+        } 
+	else {
             wait(NULL);
             write(STDOUT_FILENO,Endshell,strlen(Endshell));
         }
 				
-	}
+   }
 	return 0;
 }
