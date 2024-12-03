94c9cbcf30e3bbb16c6f738157cb2a0b7c011e95
A1eX120 <alex.iortoman@ensea.fr>
2024-12-03 09:14:37 +0100

    main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <sys/types.h>
+//#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUFSIZE 128
+
+int main(int argc, char* argv[]) {
+
+    int fd;
+    char buf[BUFSIZE];
+    ssize_t ret;
+
+    char shellstart[BUFSIZE] = "$ ./enseash\n";
+    char Outputestline1[BUFSIZE] = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
+    char endshell[BUFSIZE] = "enseash %\n";
+
+    write(STDOUT_FILENO, shellstart, BUFSIZE);
+    write(STDOUT_FILENO, Outputestline1, BUFSIZE);
+    write(STDOUT_FILENO, endshell, BUFSIZE);
+}
