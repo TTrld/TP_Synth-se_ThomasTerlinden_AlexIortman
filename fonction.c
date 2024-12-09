ae2e5de82397457de4cafffa4e4956b4b55b2874
TTrld <145064052+TTrld@users.noreply.github.com>
2024-12-09 17:31:02 +0100

    Add files via upload
    fonction pour la q4

---
+++ fonction.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "string.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <fcntl.h>
+
+int main(){
+
+int tableau[10];
+
+return tableau[11];
+
+}
